25ffa2babc9304933b1f45a47065d1563205502c
Daroxd482 <Daroxd482@gmail.com>
2023-08-21 00:42:59 -0300

    10.Javascript

---
+++ 10.Javascript/JS/práctica.js
@@ -0,0 +1,37 @@
+/* 
+var nombre = prompt("Hola, ¿cómo te llamas?")
+alert("Hola " + nombre)
+*/
+//NaN= not a number
+//Number= el método number, transforma un string que represente un número en un número (tipo de dato Number)
+/*var res1= Number(prompt("Ingrese un número"))
+var res2= Number(prompt("Ingrese otro número"))
+
+//var num1= Number(res1)
+//var num2= Number(res2)
+
+var res3= res1 + res2
+alert("la suma de los números es " + res3)*/
+
+//Pedirle al usuario que ingrese su nombre (prompt)
+//Pedirle al usuario que ingrese su edad 
+//Mostrar un mensaje (alert) que diga "hola (nombre), tu edad es (edad), naciste en el año (año de nacimiento)"
+
+/*
+var nombre= prompt("Ingrese su nombre")
+var edad= Number(prompt("Ingrese su edad"))
+var año_de_nacimiento= 2023 - edad
+alert("Hola "+ nombre +", "+ "tu edad es "+ edad + "; "+ "naciste en el año "+ año_de_nacimiento)
+*/
+
+/*
+var num= 1
+
+//incrementar variable
+num ++
+num= num + 1
+
+//decrementar variable
+num --
+num= num - 1
+*/
---
+++ 10.Javascript/JS/script.js
@@ -0,0 +1,34 @@
+//console.log () lo usamos para escribir mensajes en la consola del navegador.
+console.log("hola mundo desde un archivo externo js")
+
+//Tipos de datos
+24 //numeros
+"hola" //string (cadena de texto)
+true //booleano
+false //booleano
+
+//sumar números
+console.log(10 + 10)
+console.log(10 - 10)
+
+//sumar strings (concatenar)
+console.log("hola" + " " + "mundo")
+console.log("hola " + "mundo")
+console.log(20 + "hola")
+
+//variables
+var saludo = "hola"
+var num = 10
+
+var resultado = saludo + num
+
+console.log(saludo + " mundo")
+console.log(num+20)
+console.log(resultado)
+
+//alert
+var mensaje = "Hola mundo!!!"
+alert(mensaje)
+
+//prompt
+prompt("hola, cómo te llamas")
